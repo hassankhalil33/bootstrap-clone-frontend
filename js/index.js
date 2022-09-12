83f9bc3c8424c4634095f6cb9f75a0d22e8df5b5
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-12 23:00:38 +0300

    Added Name + Email Checker

--- js/index.js
+++ js/index.js
@@ -1,11 +1,30 @@
 // Init Variables
 
 const buttonClick = document.querySelector("#send");
+const inputName = document.querySelector(".name");
+const inputEmail = document.querySelector(".email");
+const inputPhone = document.querySelector(".phone");
+const inputMessage = document.querySelector(".message");
+const feedbackName = document.querySelector(".feedback-name");
+const feedbackEmail = document.querySelector(".feedback-email");
+const feedbackPhone = document.querySelector(".feedback-phone");
+const feedbackMessage = document.querySelector(".feedback-message");
+const regEmail = /.....@.../;
 
 // Functions
 
 function validateInputFields () {
-    console.log("WORKS!");
+    if (inputName.value.length < 5) {
+        feedbackName.textContent = "Name too short!";
+    } else {
+        feedbackName.textContent = "";
+    };
+
+    if ((inputEmail.value).match(regEmail)) {
+        feedbackEmail.textContent = "";
+    } else {
+        feedbackEmail.textContent = "Invalid Email!";
+    };
 };
 
 // Script
