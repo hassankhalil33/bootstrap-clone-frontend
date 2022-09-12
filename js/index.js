d2fda7d0e5b492554c78bd4af5dce129b2b2c1e0
hassankhalil33 <Hassan.Khalil.33@Hotmail.com>
2022-09-12 22:22:12 +0300

    Added Event Listener

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+// Init Variables
+
+const buttonClick = document.querySelector("#send");
+
+// Functions
+
+function validateInputFields () {
+    console.log("WORKS!");
+};
+
+// Script
+
+buttonClick.addEventListener("click", validateInputFields);
